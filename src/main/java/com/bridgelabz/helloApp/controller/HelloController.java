1cae90294ec48a47edcda386e1a9f6bea5977c5a
Ritick456 <ritickkr937@gmail.com>
2025-04-18 09:56:14 +0530

    [Ritick]add: created get request and used path variable

--- src/main/java/com/bridgelabz/helloApp/controller/HelloController.java
+++ src/main/java/com/bridgelabz/helloApp/controller/HelloController.java
@@ -1,10 +1,7 @@
 package com.bridgelabz.helloApp.controller;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -18,10 +15,14 @@ public class HelloController {
 
     //UC2
     @GetMapping("/query")
-    public String sayHello(@RequestParam(value = "name") String name) {
+    public String sayHelloquery(@RequestParam(value = "name") String name) {
         return "Hello " + name + " from Bridgelabz";
     }
 
-
+    //UC3
+    @GetMapping("/pathvar/{name}")
+    public String sayHellopath(@PathVariable String name){
+        return "Hello" + name + " from Bridgelabz";
+    }
 
 }
