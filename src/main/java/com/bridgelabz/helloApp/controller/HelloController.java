070190dfb8106e63e9be95eb7a28750f2a7a9f8f
Ritick456 <ritickkr937@gmail.com>
2025-04-18 09:46:55 +0530

    [Ritick]add: created query parameter

--- src/main/java/com/bridgelabz/helloApp/controller/HelloController.java
+++ src/main/java/com/bridgelabz/helloApp/controller/HelloController.java
@@ -3,15 +3,25 @@ package com.bridgelabz.helloApp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
 public class HelloController {
 
+    //UC1
     @GetMapping("/hello")
     public String getHello(){
         return "Hello";
     }
+
+    //UC2
+    @GetMapping("/query")
+    public String sayHello(@RequestParam(value = "name") String name) {
+        return "Hello " + name + " from Bridgelabz";
+    }
+
+
 
 }
